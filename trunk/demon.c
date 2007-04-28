0a1b9c8f1d7695ec91814bf8143ea95c7b99bf80
blaze <blaze@e14b36a4-afe1-4fb7-8506-8b3fee3dcf85>
2007-04-28 12:16:24 +0000

    Fix reconfig issues

--- trunk/demon.c
+++ trunk/demon.c
@@ -310,7 +310,7 @@ void ReadNewData(MYSQL *conn,MYSQL *conn2)
 				  {
 				    exec_script(UDSCRIPTFILE, users[samsuser-1].user);
 				  }  
-				sprintf(&str[0],"INSERT INTO %s.reconfig SET number='%d',action='reconfig',service='squid'",conf.samsdb,conf.cachnum);
+				sprintf(&str[0],"INSERT INTO %s.reconfig SET number='%d',action='reconfig',service='squid'",conf.samsdb,conf.cachenum);
                                 flag=send_mysql_query(conn2,&str[0]);
 				if(flag==0)
 				  {
