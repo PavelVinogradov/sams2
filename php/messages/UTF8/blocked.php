bef8a7ebf00c2d60901dbd532f985365d51203eb
PavelVinogradov <PavelVinogradov@e14b36a4-afe1-4fb7-8506-8b3fee3dcf85>
2007-05-28 08:56:33 +0000

    Update blocked.php page. (Fix #2)

--- php/messages/UTF8/blocked.php
+++ php/messages/UTF8/blocked.php
@@ -4,7 +4,6 @@
  * Author: Dmitry Chemerik chemerik@mail.ru
  * (see the file 'main.php' for license details)
  */
-
 if(isset($_GET["id"])) $id=$_GET["id"];
 if(isset($_GET["action"])) $action=$_GET["action"];
 if(isset($_GET["ip"])) $ip=$_GET["ip"];
@@ -28,18 +27,21 @@ if(isset($_GET["url"])) $url=$_GET["url"];
   $KBSIZE = $SAMSConf->KBSIZE;
   $LANG = $SAMSCong->LANG;
 
-print("  <HTML lang=\"en\"><HEAD>\n");
+  $lang="../../lang/lang.$LANG";
+  require($lang);
+/*   ************************************     */    
+  
+print("  <HTML lang=\"ru\"><HEAD>\n");
 print("  </HEAD>\n");
 print("  <TITLE>SAMS (SQUID Account Management System).</TITLE>\n");
-print("  <META  content=\"text/html; charset=$CHAR_SET\" http-equiv=\"Content-Type\">\n");
+print("  <META  content=\"text/html; charset=$LANG\" http-equiv=\"Content-Type\">\n");
 print("  <BODY>\n");
-
+print("  <TABLE BORDER=0 WIDTH=\"95%\" >");
   if($action=="sgdisable"||$action=="rejikdisable")
     {
        $result=mysql_query("SELECT squidusers.*,shablons.shour,shablons.smin,shablons.ehour,shablons.emin FROM squidusers LEFT JOIN shablons ON (squidusers.shablon=shablons.name) WHERE squidusers.nick=\"$user\" ");
        $row=mysql_fetch_array($result);
-
-       print("  <TR><TD><IMG SRC=\"$path$ICONSET/denied.gif\">");
+       print("  <TR><TD><IMG SRC=\"$path$SAMSConf->ICONSET/denied.gif\">");
        print("  <TD>");
        if(strlen($row['nick'])>0)
           print("<H2>$blocked_php_1 <FONT COLOR=\"BLUE\">$row[family] $row[name] $row[soname]($row[nick])</H2></FONT> ");
@@ -57,66 +59,66 @@ print("  <BODY>\n");
            $ksize=floor($ostatok/$SAMSConf->KBSIZE);
 	   print("  <BR><B>$blocked_php_7</B> <FONT COLOR=\"RED\"><B>$msize $blocked_php_12 $ksize $blocked_php_13</B></FONT>" );
 	 }
-
        if(strlen($user)>0)
-          print("<H2>User <FONT COLOR=\"BLUE\">$user</H2></FONT> ");
-       print("<P><FONT COLOR=\"RED\"><B><H2>Access to proxy server is denied</H2></B></FONT> ");
+          print("<H2>$blocked_php_1 <FONT COLOR=\"BLUE\">$user</H2></FONT> ");
+       print("<P><FONT COLOR=\"RED\"><B><H2>$blocked_php_2</H2></B></FONT> ");
        exit(0);
     }
   if($action=="rejikdenied"||$action=="sgdenied")
     {
-       print("  <TR><TD><IMG SRC=\"$path$ICONSET/denied.gif\">");
+       print("  <TR><TD><IMG SRC=\"$path$SAMSConf->ICONSET/denied.gif\">");
        print("  <TD> ");
-       print("<P><FONT COLOR=\"RED\"><B><H2>Access to URL <BR></FONT>");
-       print("$url<BR> <FONT COLOR=\"RED\">forbidden</H2></B></FONT> ");
+       print("<P><FONT COLOR=\"RED\"><B><H2>$blocked_php_3 <BR></FONT>");
+       print("$url<BR> <FONT COLOR=\"RED\">$blocked_php_4</H2></B></FONT> ");
        
        exit(0);
     }
-  
-      
-  print("  <TR><TD><IMG SRC=\"$path$ICONSET/denied.gif\">");
+  print("  <TR><TD><IMG SRC=\"$path$SAMSConf->ICONSET/denied.gif\">");
   $result=mysql_query("SELECT squidusers.*,shablons.shour,shablons.smin,shablons.ehour,shablons.emin FROM squidusers LEFT JOIN shablons ON (squidusers.shablon=shablons.name) WHERE id=\"$id\" ");
   $row=mysql_fetch_array($result);
 
   if($action=="userdisabled")
     {
        print("  <TD>");
-       if(strlen($row[nick])>0)
-          print("<H2>User <FONT COLOR=\"BLUE\">$row[nick]</H2></FONT> ");
-       print("<P><FONT COLOR=\"RED\"><B><H2>Access to proxy server is denied</H2></B></FONT> ");
-       if($row[enabled]<0)
+       if(strlen($row['nick'])>0)
+          print("<H2>$blocked_php_1 <FONT COLOR=\"BLUE\">$row[nick]</H2></FONT> ");
+       print("<P><FONT COLOR=\"RED\"><B><H2>$blocked_php_2</H2></B></FONT> ");
+       if($row['enabled']<0)
          {
-	   print("  <P><FONT COLOR=\"RED\"> Your account is SWITCH OFF</FONT>");
+	   print("  <P><FONT COLOR=\"RED\"> $blocked_php_5</FONT>");
 	 }
-       if($row[size]/($KBSIZE*$KBSIZE)>$row[quotes]&&$row[quotes]>0&&$row[enabled]>=0)
+       if($row['size']/($SAMSConf->KBSIZE*$SAMSConf->KBSIZE)>$row['quotes']&&$row['quotes']>0&&$row['enabled']>=0)
          {
-           print("  <P>Your traffic is OFF ");
-	   print("  <BR><B>$blocked_php_11</B> $row[quotes] $blocked_php_12");
-           $msize=floor($row['size']/($KBSIZE*$KBSIZE));
-           $ostatok=$row['size']%($KBSIZE*$KBSIZE);
-           $ksize=floor($ostatok/$KBSIZE);
+           print("  <P>$blocked_php_6 ");
+           print("  <BR><B>$blocked_php_11</B> $row[quotes] $blocked_php_12");
+           $msize=floor($row['size']/($SAMSConf->KBSIZE*$SAMSConf->KBSIZE));
+           $ostatok=$row['size']%($SAMSConf->KBSIZE*$SAMSConf->KBSIZE);
+           $ksize=floor($ostatok/$SAMSConf->KBSIZE);
 	   print("  <BR><B>$blocked_php_7</B> <FONT COLOR=\"RED\"><B>$msize $blocked_php_12 $ksize $blocked_php_13</B></FONT>" );
 	 }
     }
   if($action=="urldenied")
     {
-       if(strlen($row[nick])>0)
-         print("  <TD><H2>User <FONT COLOR=\"BLUE\">$row[nick]</H2></FONT> ");
-       print("  <P><FONT COLOR=\"RED\"><B><H3>Access to this URL is denied</H3></FONT> ");
+       if(strlen($row['nick'])>0)
+         print("  <TD><H2>$blocked_php_1 <FONT COLOR=\"BLUE\">$row[nick]</H2></FONT> ");
+       print("  <P><FONT COLOR=\"RED\"><B><H3>$blocked_php_8</H3></FONT> ");
     }
   if($action=="timedenied")
     {
-       if(strlen($row[nick])>0)
-         print("  <TD><H2>User <FONT COLOR=\"BLUE\">$row[nick]</H2></FONT> ");
-         print("  <P><FONT COLOR=\"RED\"><B><H3>time access error</H3></FONT> ");
+       if(strlen($row['nick'])>0)
+         print("  <TD><H2>$blocked_php_1 <FONT COLOR=\"BLUE\">$row[nick]</H2></FONT> ");
+         print("  <P><FONT COLOR=\"RED\"><B><H3>$blocked_php_9"); 
+         print("<BR>$blocked_php_10</H3></FONT> ");
          print("  <P>$row[shour]:$row[smin] - $row[ehour]:$row[emin] ");
     }
 
-   print("  <TR> ");
-   print("  <TD> ");
-   print("  <TD><P><H3>Access denied</H3> ");
 
-   print("  </TABLE></BODY></HTML>");
+
+  print("  <TR> ");
+  print("  <TD> ");
+  print("  <TD><P><H3>Access denied</H3> ");
+
+  print("  </TABLE></BODY></HTML>");
 
 ?>
 
