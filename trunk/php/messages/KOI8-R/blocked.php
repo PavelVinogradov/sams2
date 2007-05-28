cd75dfea42e8128e5087d33999b89de8acc030f9
PavelVinogradov <PavelVinogradov@e14b36a4-afe1-4fb7-8506-8b3fee3dcf85>
2007-05-28 08:56:33 +0000

    Update blocked.php page. (Fix #2)

--- trunk/php/messages/KOI8-R/blocked.php
+++ trunk/php/messages/KOI8-R/blocked.php
@@ -27,31 +27,37 @@ if(isset($_GET["url"])) $url=$_GET["url"];
   $KBSIZE = $SAMSConf->KBSIZE;
   $LANG = $SAMSCong->LANG;
 
-/*   ************************************       */  
-  $agent=getenv("HTTP_USER_AGENT");
-  if(strlen($agent)>2&&(strstr($agent,'windows')||strstr($agent,'Windows')))
-    {
-      //echo "<h1>OS Windows, charset cp 1251</h1>"; 
-      $LANG="WIN1251";
-    }  
-  else
-    {   
-      //echo "<h1>OS not Windows, charset koi8-r</h1>"; 
-      $LANG="KOI8-R";
-    }
   $lang="../../lang/lang.$LANG";
   require($lang);
-/*   ************************************     */    
   
-print("  <HTML><HEAD>");
-
-print("  </HEAD>");
-print("  <BODY>");
+print("  <HTML lang=\"ru\"><HEAD>\n");
+print("  </HEAD>\n");
+print("  <TITLE>SAMS (SQUID Account Management System).</TITLE>\n");
+print("  <META  content=\"text/html; charset=$LANG\" http-equiv=\"Content-Type\">\n");
+print("  <BODY>\n");
 print("  <TABLE BORDER=0 WIDTH=\"95%\" >");
   if($action=="sgdisable"||$action=="rejikdisable")
     {
+       $result=mysql_query("SELECT squidusers.*,shablons.shour,shablons.smin,shablons.ehour,shablons.emin FROM squidusers LEFT JOIN shablons ON (squidusers.shablon=shablons.name) WHERE squidusers.nick=\"$user\" ");
+       $row=mysql_fetch_array($result);
        print("  <TR><TD><IMG SRC=\"$path$SAMSConf->ICONSET/denied.gif\">");
        print("  <TD>");
+       if(strlen($row['nick'])>0)
+          print("<H2>$blocked_php_1 <FONT COLOR=\"BLUE\">$row[family] $row[name] $row[soname]($row[nick])</H2></FONT> ");
+       print("<P><FONT COLOR=\"RED\"><B><H2>$blocked_php_3 <FONT COLOR=\"BLACK\">$url</FONT> $blocked_php_4!!!</H2></B></FONT> ");
+       if($row['enabled']<0)
+         {
+	   print("  <P><FONT COLOR=\"RED\"> $blocked_php_5</FONT>");
+	 }
+       if($row['size']/($SAMSConf->KBSIZE*$SAMSConf->KBSIZE)>$row['quotes']&&$row['quotes']>0&&$row['enabled']>=0)
+         {
+           print("  <P>$blocked_php_6 ");
+           print("  <BR><B>$blocked_php_11</B> $row[quotes] $blocked_php_12");
+           $msize=floor($row['size']/($SAMSConf->KBSIZE*$SAMSConf->KBSIZE));
+           $ostatok=$row['size']%($SAMSConf->KBSIZE*$SAMSConf->KBSIZE);
+           $ksize=floor($ostatok/$SAMSConf->KBSIZE);
+	   print("  <BR><B>$blocked_php_7</B> <FONT COLOR=\"RED\"><B>$msize $blocked_php_12 $ksize $blocked_php_13</B></FONT>" );
+	 }
        if(strlen($user)>0)
           print("<H2>$blocked_php_1 <FONT COLOR=\"BLUE\">$user</H2></FONT> ");
        print("<P><FONT COLOR=\"RED\"><B><H2>$blocked_php_2</H2></B></FONT> ");
@@ -83,11 +89,11 @@ print("  <TABLE BORDER=0 WIDTH=\"95%\" >");
        if($row['size']/($SAMSConf->KBSIZE*$SAMSConf->KBSIZE)>$row['quotes']&&$row['quotes']>0&&$row['enabled']>=0)
          {
            print("  <P>$blocked_php_6 ");
-           print("  <BR><B>Ã…Õ…‘:</B> $row[quotes] Ì¬");
+           print("  <BR><B>$blocked_php_11</B> $row[quotes] $blocked_php_12");
            $msize=floor($row['size']/($SAMSConf->KBSIZE*$SAMSConf->KBSIZE));
            $ostatok=$row['size']%($SAMSConf->KBSIZE*$SAMSConf->KBSIZE);
            $ksize=floor($ostatok/$SAMSConf->KBSIZE);
-	   print("  <BR><B>$blocked_php_7</B> <FONT COLOR=\"RED\"><B>$msize Ì¬ $ksize Kb</B></FONT>" );
+	   print("  <BR><B>$blocked_php_7</B> <FONT COLOR=\"RED\"><B>$msize $blocked_php_12 $ksize $blocked_php_13</B></FONT>" );
 	 }
     }
   if($action=="urldenied")
