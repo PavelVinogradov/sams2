a1ae2f6bc9d06acb5cacf77009494166c2783175
mclight77 <mclight77@e14b36a4-afe1-4fb7-8506-8b3fee3dcf85>
2008-01-15 07:34:43 +0000

    Basic functionality with template

---
+++ branches/2.0.x/src/template.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+
+using namespace std;
+
+#include <string>
+
+#include "proxy.h"
+
+class Template{
+public:
+  Template(long id, const string & name);
+
+  ~Template();
+
+  long getId();
+  void setAuth(const string & auth);
+  void setAuth(Proxy::usrAuthType auth);
+  Proxy::usrAuthType getAuth();
+  void setQuote(long quote);
+  long getQuote();
+
+private:
+  long _id;
+  string _name;
+  Proxy::usrAuthType _auth;
+  long _quote;
+};
+
+#endif
