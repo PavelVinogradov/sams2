8bb3f8b9da5c483b2eddb0fca27b8c3bd9fc8b67
PavelVinogradov <PavelVinogradov@e14b36a4-afe1-4fb7-8506-8b3fee3dcf85>
2008-04-22 04:30:23 +0000

    Fix compilation issue. Close #169

--- branches/1.1.x/redirect.c
+++ branches/1.1.x/redirect.c
@@ -838,7 +838,7 @@ int main (int argc, char *argv[])
 		        {
 		          if( (users[USERNUMBER-1].stime>users[USERNUMBER-1].etime))
 		            { 
-		              if(users[USERNUMBER-1].stime =< (t->tm_hour*60+t->tm_min) || (t->tm_hour*60+t->tm_min) =< users[USERNUMBER-1].etime)
+		              if(users[USERNUMBER-1].stime <= (t->tm_hour*60+t->tm_min) || (t->tm_hour*60+t->tm_min) <= users[USERNUMBER-1].etime)
 			       {
                                  TIMEDENIED=0;
 			       }
