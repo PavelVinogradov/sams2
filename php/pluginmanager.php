da5a7d2e7aba9fae8c583a10513abc1508105d1e
PavelVinogradov <PavelVinogradov@e14b36a4-afe1-4fb7-8506-8b3fee3dcf85>
2008-09-01 16:43:11 +0000

    Fix case for plugin names

--- php/pluginmanager.php
+++ php/pluginmanager.php
@@ -55,7 +55,7 @@ class PluginManager {
 		$result = null;
 		
 		for ($i = 0; $i <= count($this->plugins); $i++) {
-            if ($this->plugins[$i]->name == $name)
+            if (strtolower($name) == $this->plugins[$i]->name)
 				$result =  $this->plugins[$i];
         }	
 		
