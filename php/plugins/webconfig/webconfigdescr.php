157e40708ffaa5f2c9797b0da31901225212a0ba
PavelVinogradov <PavelVinogradov@e14b36a4-afe1-4fb7-8506-8b3fee3dcf85>
2008-10-12 14:37:11 +0000

    Update plugins implementation

--- php/plugins/webconfig/webconfigdescr.php
+++ php/plugins/webconfig/webconfigdescr.php
@@ -1,22 +1,26 @@
 <?php
 
 class WebConfigDescr {
-    var $name = "webconfig";
+	var $name = "webconfig";
 	var $descr = "WEB interface settings";
-    var $author = "Pavel Vinogradov";
-    var $path = "webconfig";
-    var $content = "sysinfo";
-    var $tray = "tray";
+	var $author = "Pavel Vinogradov";
+	var $version = "1.0";
+	var $api = 2;
+	var $active = true;
+	
+	var $path = "webconfig";
+	var $content = "sysinfo";
+	var $tray = "tray";
 	var $functions = array();
 	var $classFile = "webconfigimpl.php";
 	var $className = "WebConfigImpl";
 	var $icon = "webinterface.gif";
 	
-    function WebConfigDescr() {
+	function WebConfigDescr() {
 		$this->functions[] = "sysinfo";
 		$this->functions[] = "tray";
 		$this->functions[] = "cuserdoc";
-    }
+	}
 	
 	function getInstance($db, $user, $conf, $tools) {
 		include ($this->classFile);
