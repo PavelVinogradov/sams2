45fa26c9f8918e80b8b5f70ef00026ebfa547a76
mclight77 <mclight77@e14b36a4-afe1-4fb7-8506-8b3fee3dcf85>
2008-12-25 09:07:39 +0000

    Fix #305

--- src/samsusers.cpp
+++ src/samsusers.cpp
@@ -15,6 +15,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 #include <sstream>
+#include <limits.h>
 
 #include "config.h"
 
