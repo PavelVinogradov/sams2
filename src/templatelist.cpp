efc86c3a1582331198e6ffe156cd0a0e01443177
mclight77 <mclight77@e14b36a4-afe1-4fb7-8506-8b3fee3dcf85>
2009-04-20 07:16:35 +0000

    Fix compilation error on Fedora9

--- src/templatelist.cpp
+++ src/templatelist.cpp
@@ -15,6 +15,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 #include <limits.h>
+#include <string.h>
 
 #include "config.h"
 
