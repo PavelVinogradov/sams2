1fd4c22c50a28f1eb9e44097e3136bffa2ee3d47
DmitryChemerik <DmitryChemerik@e14b36a4-afe1-4fb7-8506-8b3fee3dcf85>
2009-04-21 19:41:46 +0000

    Исправил ошибки в настройках NTLM авторизации WEB интерфейса

---
+++ php/src/authntlmbuttom_1_prop.php
@@ -0,0 +1,111 @@
+<?php
+/*  
+ * SAMS (Squid Account Management System)
+ * Author: Dmitry Chemerik chemerik@mail.ru
+ * (see the file 'main.php' for license details)
+ */
+ 
+
+
+function AuthNTLMReConfig()
+{
+  global $USERConf;
+
+echo "1 ";
+  if($USERConf->ToWebInterfaceAccess("C")!=1 )
+	exit(0);
+echo "2 ";
+  UpdateAuthParameter("ntlm","ntlmserver");
+  UpdateAuthParameter("ntlm","ntlmdomain");
+  UpdateAuthParameter("ntlm","ntlmadmin");
+  UpdateAuthParameter("ntlm","ntlmadminpasswd");
+  UpdateAuthParameter("ntlm","ntlmusergroup");
+
+}
+
+function AuthNTLMReConfigForm()
+{
+  global $SAMSConf;
+  global $PROXYConf;
+  global $USERConf;
+  $DB=new SAMSDB(&$SAMSConf);
+
+  $files=array();
+  if(isset($_GET["id"])) $proxy_id=$_GET["id"];
+   
+  $lang="./lang/lang.$SAMSConf->LANG";
+  require($lang);
+  if($USERConf->ToWebInterfaceAccess("C")!=1 )
+	exit;
+  
+
+  PageTop("config_48.jpg","NTLM Configuration ");
+  print("<P>\n");
+
+
+  print("<FORM NAME=\"adldreconfigform\" ACTION=\"main.php\">\n");
+  print("<BR><INPUT TYPE=\"SUBMIT\" value=\"$adminbuttom_1_prop_SamsReConfigForm_12\">\n");
+  print("<INPUT TYPE=\"HIDDEN\" NAME=\"show\" value=\"exe\">\n");
+  print("<INPUT TYPE=\"HIDDEN\" NAME=\"function\" value=\"authntlmreconfig\">\n");
+  print("<INPUT TYPE=\"HIDDEN\" NAME=\"filename\" value=\"authntlmbuttom_1_prop.php\">\n");
+
+  print("<TABLE CLASS=samstable WIDTH=\"90%\" BORDER=0>\n");
+
+
+  print("<TR bgcolor=blanchedalmond>\n");
+  print("<TD><B>Windows domain server</B>\n");
+  $value=GetAuthParameter("ntlm","ntlmserver");
+  print("<TD><INPUT TYPE=\"TEXT\" NAME=\"ntlmserver\" VALUE=\"$value\" >\n");
+
+  print("<TR bgcolor=blanchedalmond>\n");
+  print("<TD><B>Windows domain</B>\n");
+  $value=GetAuthParameter("ntlm","ntlmdomain");
+  print("<TD><INPUT TYPE=\"TEXT\" NAME=\"ntlmdomain\" VALUE=\"$value\">\n");
+
+  print("<TR bgcolor=blanchedalmond>\n");
+  print("<TD><B>Windows domain administrator</B>\n");
+  $value=GetAuthParameter("ntlm","ntlmadmin");
+  print("<TD><INPUT TYPE=\"TEXT\" NAME=\"ntlmadmin\" VALUE=\"$value\">\n");
+
+  print("<TR bgcolor=blanchedalmond>\n");
+  print("<TD><B>Windows domain administrator password</B>\n");
+  $value=GetAuthParameter("ntlm","ntlmadminpasswd");
+  print("<TD><INPUT TYPE=\"TEXT\" NAME=\"ntlmadminpasswd\" VALUE=\"$value\">\n");
+
+  print("<TR bgcolor=blanchedalmond>\n");
+  print("<TD><B>Windows domain user group</B>\n");
+  $value=GetAuthParameter("ntlm","ntlmusergroup");
+  print("<TD><INPUT TYPE=\"TEXT\" NAME=\"ntlmusergroup\" VALUE=\"$value\">\n");
+
+  print("</TABLE>\n");
+  print("<BR><INPUT TYPE=\"SUBMIT\" value=\"$adminbuttom_1_prop_SamsReConfigForm_12\">\n");
+  print("</FORM>\n");
+}
+
+
+
+function authntlmbuttom_1_prop()
+{
+  global $SAMSConf;
+  global $USERConf;
+
+ if(isset($_GET["id"])) $id=$_GET["id"];
+  
+  $lang="./lang/lang.$SAMSConf->LANG";
+  require($lang);
+
+  if($USERConf->ToWebInterfaceAccess("C")==1 )
+    {
+       GraphButton("main.php?show=exe&function=authntlmreconfigform&filename=authntlmbuttom_1_prop.php",
+	               "basefrm","config_32.jpg","config_48.jpg","NTLM authorisation configure");
+    }
+
+}
+
+
+
+
+
+
+
+?>
--- php/src/authntlmtray.php
+++ php/src/authntlmtray.php
@@ -5,8 +5,95 @@
  * (see the file 'main.php' for license details)
  */
 
+function NTLMtest()
+{
+  global $SAMSConf;
+  global $USERConf;
+
+  if($USERConf->ToWebInterfaceAccess("C")!=1 )
+	exit;
+
+  print("<H1>Test NTLM connection</H1>");
+
+  $ntlmserver=GetAuthParameter("ntlm","ntlmserver");
+  $ntlmdomain=GetAuthParameter("ntlm","ntlmdomain");
+  $ntlmadmin=GetAuthParameter("ntlm","ntlmadmin");
+  $ntlmpasswd=GetAuthParameter("ntlm","ntlmadminpasswd");
+
+  $users=ExecuteShellScript("getntlmgroups","$ntlmserver $ntlmadmin $ntlmpasswd");
+  $a=explode("|",$users);
+  $acount=count($a);
+
+  print("<TABLE CLASS=samstable>");
+  print("<TH width=5%>No");
+  print("<TH width=95%>NTLM groups");
+  for($i=0;$i<$acount;$i++)
+	echo("<TR><TD>$i<TD>$a[$i]<BR>\n");
+  echo "</TABLE><P>";
+
+  $users=ExecuteShellScript("getntlmusers","$ntlmserver $ntlmadmin $ntlmpasswd");
+  $a=explode("|",$users);
+  $acount=count($a);
+
+  print("<TABLE CLASS=samstable>");
+  print("<TH width=5%>No");
+  print("<TH width=95%>NTLM users");
+  for($i=0;$i<$acount;$i++)
+	echo("<TR><TD>$i<TD>$a[$i]<BR>\n");
+  echo "</TABLE>";
+
+
+}   
  
 
+function AuthNTLMValues()
+{
+  global $USERConf;
+  if($USERConf->ToWebInterfaceAccess("C")!=1 )
+	exit;
+
+  PageTop("config_48.jpg","NTLM configuration ");
+  print("<P>\n");
+
+  print("<TABLE CLASS=samstable WIDTH=\"90%\" BORDER=0>\n");
+
+  print("<TR bgcolor=blanchedalmond>\n");
+  print("<TD><B>Windows domain server</B>\n");
+  $value=GetAuthParameter("ntlm","ntlmserver");
+  print("<TD>$value \n");
+
+  print("<TR bgcolor=blanchedalmond>\n");
+  print("<TD><B>Windows domain</B>\n");
+  $value=GetAuthParameter("ntlm","ntlmdomain");
+  print("<TD>$value \n");
+
+  print("<TR bgcolor=blanchedalmond>\n");
+  print("<TD><B>Windows domain administrator</B>\n");
+  $value=GetAuthParameter("ntlm","ntlmadmin");
+  print("<TD>$value \n");
+
+  print("<TR bgcolor=blanchedalmond>\n");
+  print("<TD><B>Windows domain administrator password</B>\n");
+  $value=GetAuthParameter("ntlm","ntlmadminpasswd");
+  print("<TD>$value \n");
+
+  print("<TR bgcolor=blanchedalmond>\n");
+  print("<TD><B>Windows domain user group</B>\n");
+  $value=GetAuthParameter("ntlm","ntlmusergroup");
+  print("<TD>$value \n");
+
+
+  print("</TABLE>\n");
+
+  print("<FORM NAME=\"adldreconfigform\" ACTION=\"main.php\">\n");
+  print("<INPUT TYPE=\"HIDDEN\" NAME=\"show\" value=\"exe\">\n");
+  print("<INPUT TYPE=\"HIDDEN\" NAME=\"function\" value=\"ntlmtest\">\n");
+  print("<INPUT TYPE=\"HIDDEN\" NAME=\"filename\" value=\"authntlmtray.php\">\n");
+
+  print("<BR><INPUT TYPE=\"SUBMIT\" value=\"test ntlm configurations\">\n");
+  print("</FORM>\n");
+
+}
 
 
 
@@ -20,7 +107,7 @@ function AuthNTLMTray()
   require($lang);
 
       print("<SCRIPT>\n");
-      print("        parent.basefrm.location.href=\"main.php\";\n");
+      print("        parent.basefrm.location.href=\"main.php?show=exe&function=authntlmvalues&filename=authntlmtray.php\";\n");
       print("</SCRIPT> \n");
 
   if($USERConf->ToWebInterfaceAccess("C")==1 )
@@ -28,9 +115,9 @@ function AuthNTLMTray()
 	print("<TABLE border=0 WIDTH=95%>\n");
 	print("<TR HEIGHT=60>\n");
 	print("<TD WIDTH=25%>");
-	print("<B><FONT SIZE=\"+1\">ADLD</FONT></B>\n");
+	print("<B><FONT SIZE=\"+1\">NTLM</FONT></B>\n");
 
-	ExecuteFunctions("./src", "authadldbuttom","1");
+	ExecuteFunctions("./src", "authntlmbuttom","1");
 
      }
   print("<TD>\n");
