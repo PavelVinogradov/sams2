98bd4c0f5a659c0f9bbdc62922ee12054888aa53
DmitryChemerik <DmitryChemerik@e14b36a4-afe1-4fb7-8506-8b3fee3dcf85>
2009-11-10 20:28:39 +0000

    Добавлен запрос на активицию пользователя в функцию очистки счетчика траффика пользователя

--- php/src/userbuttom_5_clear.php
+++ php/src/userbuttom_5_clear.php
@@ -17,16 +17,51 @@ function ClearUserTrafficCounter()
 	exit(0);
   
   if(isset($_GET["id"])) $id=$_GET["id"];
+  if(isset($_GET["enabled"])) $enabled=$_GET["enabled"];
 
-  $QUERY="UPDATE squiduser SET s_size='0',s_hit='0' WHERE s_user_id='$id'";
+  if($enabled=="on")
+	$uenabled=",s_enabled='1'";
+  else
+	$uenabled="";
+
+  $QUERY="UPDATE squiduser SET s_size='0',s_hit='0'".$uenabled." WHERE s_user_id='$id'";
   $num_rows=$DB->samsdb_query_value($QUERY);
   print("<SCRIPT>\n");
   print("        parent.tray.location.href=\"tray.php?show=exe&filename=usertray.php&function=usertray&auth=ip&id=$id\";\n");
-//tray.php?show=exe&filename=usertray.php&function=usertray&id=$row_[s_user_id]&auth=$row_[s_auth]
+  if($enabled=="on")
+  print("        parent.lframe.location.href=\"lframe.php\";\n");
   print("</SCRIPT> \n");
 
 }
 
+function ClearUserTrafficCounterForm()
+{
+  global $SAMSConf;
+  global $USERConf;
+  global $SquidUSERConf;
+
+  $DB=new SAMSDB(&$SAMSConf);
+  $lang="./lang/lang.$SAMSConf->LANG";
+  require($lang);
+  if($USERConf->ToWebInterfaceAccess("UC")!=1)
+	exit(0);
+  
+  if(isset($_GET["id"])) $id=$_GET["id"];
+  $SquidUSERConf=new SAMSUSER();
+  $SquidUSERConf->sams_user($id);
+
+	PageTop("usertraffic_48.jpg","$usertray_UserTray_1 <FONT COLOR=\"BLUE\">$SquidUSERConf->s_nick</FONT> <BR>$userbuttom_5_clear_userbuttom_5_clear_1");
+
+	print("<FORM NAME=\"UserIDForm\" ACTION=\"main.php\">\n");
+	print("<INPUT TYPE=\"HIDDEN\" NAME=\"id\" id=UserName value=\"$SquidUSERConf->s_user_id\">\n");
+	print("<INPUT TYPE=\"HIDDEN\" NAME=\"show\" id=Show value=\"exe\">\n");
+	print("<INPUT TYPE=\"HIDDEN\" NAME=\"function\" id=function value=\"clearusertrafficcounter\">\n");
+	print("<INPUT TYPE=\"HIDDEN\" NAME=\"filename\" id=filename value=\"userbuttom_5_clear.php\">\n");
+	print("<BR><INPUT TYPE=\"CHECKBOX\" NAME=\"enabled\" CHECKED> $activate_user\n");
+	print("<BR><INPUT TYPE=\"SUBMIT\" value=\"$userbuttom_5_clear_userbuttom_5_clear_1\">\n");
+	print("</FORM>\n");
+
+}
 
 
 
@@ -41,28 +76,7 @@ function userbuttom_5_clear($userid)
 
   if($USERConf->ToWebInterfaceAccess("UC")==1)
     {
-	print("<SCRIPT language=JAVASCRIPT>\n");
-	print("function ReloadBaseFrame()\n");
-	print("{\n");
-	print("   window.location.reload();\n");
-	print("}\n");
-	print("function ClearCounter(username,userid)\n");
-	print("{\n");
-	print("  value=window.confirm(\"$userbuttom_5_clear_userbuttom_5_clear_1 $SquidUSERConf->s_nick? \");\n");
-	print("  if(value==true) \n");
-	print("     {\n");
-	print("        parent.basefrm.location.href=\"main.php?show=exe&function=clearusertrafficcounter&filename=userbuttom_5_clear.php&id=$SquidUSERConf->s_user_id\";\n");
-	print("        window.setInterval(\"ReloadBaseFrame()\",500)\n");
-	print("     }\n");
-	print("}\n");
-	print("</SCRIPT> \n");
-
-	print("<TD CLASS=\"samstraytd\" >\n");
-	print("<IMAGE id=Trash name=\"Clear\" src=\"$SAMSConf->ICONSET/erase_32.jpg\" \n ");
-	print("TITLE=\"$userbuttom_5_clear_userbuttom_5_clear_1\"  border=0 ");
-	print("onclick=ClearCounter() \n");
-	print("onmouseover=\"this.src='$SAMSConf->ICONSET/erase_48.jpg'\" \n");
-	print("onmouseout= \"this.src='$SAMSConf->ICONSET/erase_32.jpg'\" >\n");
+	GraphButton("main.php?show=exe&function=clearusertrafficcounterform&filename=userbuttom_5_clear.php&id=$SquidUSERConf->s_user_id","basefrm","erase_32.jpg","erase_48.jpg","$userbuttom_5_clear_userbuttom_5_clear_1");
     }
 
 }
