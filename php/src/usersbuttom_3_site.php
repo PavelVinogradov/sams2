f561518275fb997afc9b729fafc1f375e17e98dd
DmitryChemerik <DmitryChemerik@e14b36a4-afe1-4fb7-8506-8b3fee3dcf85>
2010-04-15 19:52:56 +0000

    Изменения в отчете по посещенным пользователями сайтам

--- php/src/usersbuttom_3_site.php
+++ php/src/usersbuttom_3_site.php
@@ -174,7 +174,7 @@ function UsersSitesPeriod()
   print("<TH WIDTH=20%>URL");
   if($USERConf->ToWebInterfaceAccess("C")==1)
   {
-      print("<TH WIDTH=15%>");
+      print("<TH WIDTH=15%>$userbuttom_4_site_UserSitesPeriod_3");
       print("<TH WIDTH=15%>$userbuttom_4_site_UserSitesPeriod_4");
    }   
   print("<TH WIDTH=15%>$userbuttom_4_site_UserSitesPeriod_5");
