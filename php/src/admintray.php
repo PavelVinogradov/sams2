b83534e52a4337f6bee4b52107023446b6bec69d
surae <surae@yandex.ru>
2011-10-01 07:04:42 +0000

    My first commit - small interface translation

--- php/src/admintray.php
+++ php/src/admintray.php
@@ -26,8 +26,8 @@ function AdminTray()
 
 	if($USERConf->ToWebInterfaceAccess("C")!=1 )
 	{
-		print("<TD WIDTH=34 HEIGH=34><IMAGE src=\"$SAMSConf->ICONSET/config_32.jpg\" BORDER=0 ALT=\"SAMS management interface\"\n ");
-		print("<TD><A HREF=\"main.php?show=exe&function=changeuser&filename=adminbuttom_2_chuser.php\" TARGET=basefrm> SAMS management interface</A>");
+		print("<TD WIDTH=34 HEIGH=34><IMAGE src=\"$SAMSConf->ICONSET/config_32.jpg\" BORDER=0 ALT=$SAMS_management_interface\n ");
+		print("<TD><A HREF=\"main.php?show=exe&function=changeuser&filename=adminbuttom_2_chuser.php\" TARGET=basefrm>$SAMS_management_interface</A>");
 	}
 	else
 	{
